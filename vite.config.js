1d71cf278710da04986953d88cd4469c21baa0db
Himanshu-Lilhore <himanshulilhore@gmail.com>
2024-03-30 03:49:19 +0530

    correcting dep err

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/[Randomizer-animation]/"
+  base: "/Randomizer-animation/"
 })
